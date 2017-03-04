f47b8ff5d15904694960de6e67e890cf298b81f7
lyyrwx <18306482534@163.com>
2017-03-04 13:48:41 +0800

    优化样式

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { DatePicker } from 'antd';
+import { DatePicker, message } from 'antd';
 
 function disabledDate(current) {
   var today = new Date();
   today.setDate(today.getDate() - 1);
-  return current && (current.valueOf() < today.getTime() || current.valueOf() > Date.parse("2017-08-31"))
+  return current && (current.valueOf() < today.getTime() || current.valueOf() > Date.parse("2017-09-01"))
 }
-function App() {
-  return (
-    <div style={{ margin: 200 }}>
-      <DatePicker
-          format="YYYY-MM-DD"
-          disabledDate={disabledDate}
-      />
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    handleChange(date) {
+        message.info('您选择的日期是: ' + date.toString());
+    }
+    render() {
+        return (
+            <div style={{ width: 400, margin: '100px auto' }}>
+                <DatePicker
+                    format="YYYY-MM-DD"
+                    disabledDate={disabledDate}
+                    onChange={value => this.handleChange(value)}
+                />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
